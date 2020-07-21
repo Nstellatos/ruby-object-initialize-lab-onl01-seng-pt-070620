0c678620b48c76db11ac1ee4d72da8d652704d41
Nicole Stellatos <stellatosnicole@gmail.com>
2020-07-21 23:14:18 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,14 +1,18 @@
-class Dog 
-  def initialize(breed,dogs_name=)
-    @breed= breed
-    @name= dogs_name
+class Dog
+  def initialize (dogs_name, dogs_breed = "Mutt")
+    @name = dogs_name
+    @breed = dogs_breed
   end
-  
-  def breed=(breed)
-    @breed=breed
+
+  def name=(dogs_name, dogs_breed = "Mutt")
+    @name = dogs_name
+    @breed = dogs_breed
   end
-  
+
+  def name
+    @name
+  end
   def breed
     @breed
-  end
+  end 
 end
